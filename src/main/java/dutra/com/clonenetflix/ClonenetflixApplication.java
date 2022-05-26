6c9700684293825b0057cf906b78a10005fa11ad
Edimilson de Lima <edimilsonldutra@hotmail.com>
2022-05-25 22:47:02 -0300

    Projeto criado

---
+++ src/main/java/dutra/com/clonenetflix/ClonenetflixApplication.java
@@ -0,0 +1,13 @@
+package dutra.com.clonenetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonenetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonenetflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dutra/com/clonenetflix/ClonenetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package dutra.com.clonenetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonenetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
